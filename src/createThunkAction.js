5d26e24bfe1d7230c14b8f88af60d5aeef3d7e2f
Victor Saiz <vs@vsaiz.me>
2017-11-11 22:34:13 +0100

    add symbol to thunkAction

--- src/createThunkAction.js
+++ src/createThunkAction.js
@@ -2,6 +2,6 @@ import createAction from './createAction';
 
 export default (name, thunkAction, metaCreator) => {
   if (!thunkAction) return createAction(name, null, metaCreator);
-  thunkAction.toString = () => name;
+  thunkAction.toString = () => Symbol(name);
   return thunkAction;
 };
