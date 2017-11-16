8b2867827ebf3ff918b5bcb03c85a3fe14d36d63
Victor Saiz <vs@vsaiz.me>
2017-11-16 14:32:34 +0100

    add rollup build chain

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: './lib/index.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve({
+      modulesOnly: true,
+      extensions: ['.js']
+    }),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
+};
