2478a8bbc3694e8982e6886cd353f226befc60d5
Victor Saiz <vs@vsaiz.me>
2017-11-22 12:42:37 +0100

    add missing getState cparam

--- src/createThunkAction.js
+++ src/createThunkAction.js
@@ -4,9 +4,9 @@ export default (name, thunkAction, metaCreator) => {
   const action = createAction(name, null, metaCreator);
   if (!thunkAction) return action;
 
-  const returnAction = payload => (dispatch, state) => {
+  const returnAction = payload => (dispatch, getState) => {
     dispatch(action());
-    return thunkAction(payload)(dispatch, state);
+    return thunkAction(payload)(dispatch, getState);
   };
 
   returnAction.toString = () => Symbol(name);
