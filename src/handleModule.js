c2c12a502ad67ab257ec01e4d6be5ae79c876751
Victor Saiz <vs@vsaiz.me>
2018-01-03 16:52:10 +0100

    small reducer fix

--- src/handleModule.js
+++ src/handleModule.js
@@ -2,5 +2,9 @@ import handleActions from './handleActions';
 
 export default function handleModule(module) {
   const { reducers, initialState } = module;
-  return handleActions(reducers, initialState || {});
+  if (!reducers.default) {
+    return console
+      .warn('You are attempting to connect a module that doesn\'t export any default reducers.');
+  }
+  return handleActions(reducers.default || reducers, initialState || {});
 }
